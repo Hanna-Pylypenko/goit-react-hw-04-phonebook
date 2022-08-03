f4c75f6d650e8e9311ab663053e724daaab588e5
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-08-03 18:49:12 +0300

    figured out the useLocalStorage hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,47 +1,43 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ContactsList } from './ContactsList/ContactsList';
 import { nanoid } from 'nanoid';
 import { ContactsForm } from './ContactsForm/ContactsForm';
 import { Filter } from './Filter/Filter';
+const useLocalStorage = (key, defaultValue) => {
+  const [state, setState] = useState(() => {
+    return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
+  });
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state));
+  }, [state, key]);
+  return [state, setState];
+};
+export const App = () => {
+  const [contacts, setContacts] = useLocalStorage('contacts', [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-  componentDidUpdate(_, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-  componentDidMount() {
-    if (localStorage.getItem('contacts')) {
-      const contacts = localStorage.getItem('contacts');
-      const parsedContacts = JSON.parse(contacts);
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-  onFilterHandler = evt => {
-    this.setState({ filter: evt.currentTarget.value });
+  const onFilterHandler = evt => {
+    setFilter(evt.currentTarget.value);
   };
 
-  filterContacts = () => {
-    return this.state.contacts.filter(contact =>
-      contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+  const filterContacts = () => {
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
     );
+    return filteredContacts;
   };
-  onSubmitHandler = ({ name, number }) => {
+  const onSubmitHandler = ({ name, number }) => {
     let contactData = {
       id: nanoid(),
       name: name,
       number: number,
     };
-    this.state.contacts.forEach(contact => {
+    contacts.forEach(contact => {
       if (contact.name.toLowerCase() === name.toLowerCase()) {
         alert(`${name} is already in contacts`);
         contactData = {
@@ -51,40 +47,32 @@ export class App extends Component {
         };
       }
     });
-    contactData.id &&
-      this.setState(prevState => ({
-        contacts: [...prevState.contacts, contactData],
-      }));
+    contactData.id && setContacts(prevState => [...prevState, contactData]);
   };
-  deleteItem = itemId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== itemId),
-    }));
-  };
-
-  render() {
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'flex-start',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <h1>Phonebook</h1>
-        <ContactsForm onSubmit={this.onSubmitHandler} />
-
-        <h2>Contacts</h2>
-        <Filter value={this.state.filter} onChange={this.onFilterHandler} />
-        <ContactsList
-          deleteItem={this.deleteItem}
-          filteredItems={this.filterContacts()}
-        />
-      </div>
+  const deleteItem = itemId => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== itemId)
     );
-  }
-}
+  };
+
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <h1>Phonebook</h1>
+      <ContactsForm onSubmit={onSubmitHandler} />
+
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={onFilterHandler} />
+      <ContactsList deleteItem={deleteItem} filteredItems={filterContacts()} />
+    </div>
+  );
+};
